85179e494f7058d3a32cadb769b24fb1c325f21a
dae1804 <dae1804@cs.rit.edu>
2010-05-03 18:40:16 -0700

    Added sample insert to student.  We'll want to change this / add more later...

--- sql/insert-data.sql
+++ sql/insert-data.sql
@@ -1,7 +1,3 @@
-
-create table
-CREATE TABLE t (
-	a number,
-	b varchar2(10)
-)
+insert into student (bannerNo, lastName, firstName, middleName, DOB, street, city, postCode, email, mobilePhone, major, minor, category, nationality, specialNeeds, comments) values ( 
+'A0001', 'Smith', 'John', 'Q', to_date('1990-01-01', 'yyyy-mm-dd'), '1 Test Dr.', 'Testville', 12345, 'test@drive.com', 012-345-6789', 'Computer Science', 'Database Design', 'Undergrad', 'US', '', '');
 
