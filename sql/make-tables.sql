e46bf9ac5c5ea008d75d095273407cf71cb9d7fc
dae1804 <dae1804@cs.rit.edu>
2010-05-03 19:09:22 -0700

    Added advisor, next_of_kin, has_next_of_kin

--- sql/make-tables.sql
+++ sql/make-tables.sql
@@ -3,10 +3,10 @@ DROP TABLE Advisor;
 DROP TABLE NextOfKin; 
 
 CREATE TABLE Student (
-	bannerNo VARCHAR2(20) PRIMARY KEY,
-	lastName VARCHAR(30),
-	firstName VARCHAR(30),
-	middleName VARCHAR(30),
+	bannerNo VARCHAR2(12) PRIMARY KEY,
+	lastName VARCHAR2(20),
+	firstName VARCHAR2(20),
+	middleName VARCHAR2(20),
 	dob DATE,
 	street VARCHAR2(25),
 	city VARCHAR2(20),
@@ -18,26 +18,32 @@ CREATE TABLE Student (
 	category VARCHAR2(20),
 	nationality VARCHAR2(20),
 	specialNeeds CLOB, 
-	comments CLOB
+	comments CLOB,
+	advisorEmail VARCHAR2(30)
 );
 
-
 CREATE TABLE Advisor (
-	email VARCHAR2(20) PRIMARY KEY,
-	name VARCHAR2(50),
+	email VARCHAR2(30) PRIMARY KEY,
+	lastName VARCHAR2(20),
+	firstName VARCHAR2(20),
+	middleName VARCHAR2(20),
 	position VARCHAR2(20),
 	department VARCHAR2(20),
-	extension VARCHAR2(4),
+	extension VARCHAR2(6,0),
 	room VARCHAR2(8)
 );
 
-
-CREATE TABLE NextOfKin (
+CREATE TABLE Next_Of_Kin (
 	name VARCHAR2(50) PRIMARY KEY,
-	relationship VARCHAR2(50),
+	contactTelNo VARCHAR2(12) PRIMARY KEY,
 	city VARCHAR2(20),
 	street VARCHAR2(25),
-	postCode NUMBER(1,5),
-	contactTelNo VARCHAR2(12)
+	postCode NUMBER(1,5)
 );
-	
+
+CREATE TABLE Has_Next_Of_Kin (
+	name VARCHAR2(50) PRIMARY KEY,
+	contactTelNo VARCHAR2(12) PRIMARY KEY,
+	bannerNo VARCHAR2(12) PRIMARY KEY,
+	relationship VARCHAR2(25)
+);
