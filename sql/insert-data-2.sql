135fa685c1acc51dc71f91aa921032638b43f252
p485-70c <p485-70c@holly.(none)>
2010-05-04 13:24:55 -0400

    Added script to insert full set of data.

---
+++ sql/insert-data-2.sql
@@ -0,0 +1,7 @@
+INSERT INTO Advisor (email, lastName, firstName, middleName, position, department, extension, extension, room) VALUES ('ahughes@sample.edu', 'Hughes', 'Autin', 'A', 'Advisor', 'CompSci', '1001', '04-101A');
+INSERT INTO Advisor (email, lastName, firstName, middleName, position, department, extension, extension, room) VALUES ('hhart@sample.edu', 'Hart', 'Hailey', 'K', 'Advisor', 'CompSci', '2002', '04-102A');
+INSERT INTO Advisor (email, lastName, firstName, middleName, position, department, extension, extension, room) VALUES ('baustin@sample.edu', 'Austin', 'Brayden', null, 'Advisor', 'IT', '3003', '04-103A');
+INSERT INTO Advisor (email, lastName, firstName, middleName, position, department, extension, extension, room) VALUES ('agonzale@sample.edu', 'Gonzalez', 'Abigail', 'Ava', 'Advisor', 'Acting', '4004', '04-104A');
+INSERT INTO Advisor (email, lastName, firstName, middleName, position, department, extension, extension, room) VALUES ('dlopez@sample.edu', 'Lopez', 'Dylan', null, 'Advisor', 'Physics', '5005', '04-105A');
+INSERT INTO Advisor (email, lastName, firstName, middleName, position, department, extension, extension, room) VALUES ('ocunning@sample.edu', 'Cunningham', 'Owen', null, 'Advisor', 'LibArts', '6006', '04-106A');
+
